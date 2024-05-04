189f4c80f62dd9f1554e3146d10dc6561a6708eb
alexya <alexya@outlook.com>
2024-05-04 09:32:12 +0800

    add a new plugin treesj

--- .config/nvim/lua/josean/plugins/utils.lua
+++ .config/nvim/lua/josean/plugins/utils.lua
@@ -16,6 +16,22 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
   },
+  -- join or split a clode block e.g. class, objuect, function, if-condiftion, etc.
+  {
+    "Wansmer/treesj",
+    event = "VeryLazy",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("treesj").setup({
+        use_default_keymaps = false,
+        max_join_length = 240,
+      })
+
+      vim.keymap.set("n", "<leader>jt", function()
+        require("treesj").toggle({ split = { recursive = true } })
+      end, { desc = "Toggle the merge/split code block" })
+    end,
+  },
   -- Change the colors, opacity of the theme
   -- {
   --   "mvllow/modes.nvim",
