8e65b5938f0c48fe951b9bd016bcf9194c1193fd
alexya <alexya@outlook.com>
2024-05-17 22:55:35 +0800

    Fix two issues of the Neovim plugins running on winOS

--- .config/nvim/lua/josean/plugins/bufferline.lua
+++ .config/nvim/lua/josean/plugins/bufferline.lua
@@ -2,6 +2,7 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
+  -- tag = "v4.5.2",  -- to resolve a runtime issue on Windows OS, and need to comment out above line version = "*"
   opts = {
     options = {
       mode = "tabs",
